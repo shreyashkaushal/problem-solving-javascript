76d1aa6c1c9a3feafb5979c7164d86d7dea3b4e8
shreyashkaushal <shreyashkaushal10@gmail.com>
2019-09-27 13:14:14 +0530

    fruit counting game

---
+++ fruitcountinggame.js
@@ -0,0 +1,14 @@
+function addFruit(fruitNumber)
+{
+    if(fruitNumber== undefined )
+    {
+        console.log("no fruit provided , give me some fruits");
+
+    }
+    else{
+        console.log("the total no. of fruits are " +fruitNumber);
+    }
+
+}
+addFruit(10);
+addFruit();
