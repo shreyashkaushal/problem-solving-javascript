5a4c0d34a7c6ef346d04e3ffe7690c2b2ef95cf4
shreyashkaushal <shreyashkaushal10@gmail.com>
2019-09-30 13:22:47 +0530

    string reverse

---
+++ stringreversal.js
@@ -0,0 +1,25 @@
+//first solution
+/*var a = "apple";
+var b=[];
+var j = 0;
+for(i=(a.length)-1;i>=0;i--)
+{
+    
+    b[j]= a[i];
+    j++
+}
+
+console.log(b.join(''));
+*/
+//second solution
+/*function reverse(str)
+{
+    let reversed ='';
+    for(let character of str)
+{
+    reversed= character + reversed;
+}
+return reversed
+}
+console.log(reverse('shreyash'));
+*/
